01f3f7549e9ee7fb4c07d207785061bfdc9a8295
Gropniik <35626615+Bapt-iste@users.noreply.github.com>
2020-01-02 21:35:27 +0100

    Unstable version
    This doesn't work.

---
+++ JeuDuPendu/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuDuPendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> mots = new List<string>();
+            Console.WriteLine("Choisissez un mot :");
+            string wordToGuessTmp = Console.ReadLine();
+            char[] wordToGuess = new char[wordToGuessTmp.Length];
+            char[] wordGuessed = new char[wordToGuessTmp.Length];
+            char userInput;
+            Console.Clear();
+
+            for (int i = 0; i < wordToGuessTmp.Length; i++)
+            {
+                wordToGuess[i] = wordToGuessTmp[i];
+                wordGuessed[i] = (char)35;
+            }
+
+            while (!(wordToGuess.Equals(wordGuessed)))
+            {
+                Console.WriteLine("\n*********************");
+                Console.WriteLine("Choisissez une lettre : ");
+                userInput = Convert.ToChar(Console.ReadLine());
+                Console.Write("Ton mot : ");
+                for(int i = 0; i < wordToGuessTmp.Length; i++)
+                {
+                    if (wordToGuess[i].Equals(userInput))
+                    {
+                        wordGuessed[i] = userInput;
+                    }
+                    else
+                    {
+                        
+                    }
+                    Console.Write(wordGuessed[i]);
+                }
+
+                
+
+            }
+
+        }
+    }
+}
